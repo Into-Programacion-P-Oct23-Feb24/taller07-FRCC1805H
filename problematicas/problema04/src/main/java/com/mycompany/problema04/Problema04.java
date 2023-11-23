fb0fc0337a3f57f57977f00fbe3c12f087c6aa55
fabian <frcanar2@utpl.edu.ec>
2023-11-23 18:02:37 -0500

    taller07

---
+++ problematicas/problema04/src/main/java/com/mycompany/problema04/Problema04.java
@@ -0,0 +1,16 @@
+/*
+ *  to change this license
+ */
+
+package com.mycompany.problema04;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Problema04 {
+
+    public static void main(String[] args) {
+      
+    }
+}
